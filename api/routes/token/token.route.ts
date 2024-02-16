b970e002adb4e2a4b51386f74d795f9ddfb0d08f
Gilles COGNIN <gillescognin@gmail.com>
2024-02-16 07:54:49 +0100

    feat: Updating token route name

--- api/routes/token/token.route.ts
+++ api/routes/token/token.route.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import generateToken from "./generateToken.ctrl";
+import generateToken from "./token.ctrl";
 
 export default async (req: Request, res: Response, next: NextFunction) => {
   console.log("Corps de la requête :", req.body);
