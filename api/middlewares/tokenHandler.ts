10d07d6e47a793c2aa991ac1bf7e23e0d172df75
Gilles COGNIN <gillescognin@gmail.com>
2024-02-16 22:18:30 +0100

    feat: Enabling token validation in token middleware

--- api/middlewares/tokenHandler.ts
+++ api/middlewares/tokenHandler.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from "express";
 import { CustomError } from "./errorHandler";
 
 interface RequestWithUser extends Request {
-  userId?: string;
+  user?: JwtPayload;
 }
 
 export function tokenHandler(
@@ -11,7 +11,17 @@ export function tokenHandler(
   res: Response,
   next: NextFunction
 ) {
+<<<<<<< HEAD
   const token = req.headers["authorization"];
+=======
+  const token = req.headers["authorization"]?.slice(
+    7,
+    req.headers["authorization"]?.length
+  );
+  console.log("Token:", token);
+  console.log("Original TOKEN", process.env.JWT_SECRET);
+
+>>>>>>> 58d1ca4 (feat: Enabling token validation in token middleware)
   if (!token) {
     return next(
       new CustomError(403, ["No authentication token given."], {
@@ -23,6 +33,19 @@ export function tokenHandler(
   }
 
   jwt.verify(token, process.env.JWT_SECRET as string, (err: any, decoded) => {
+<<<<<<< HEAD
+=======
+    console.log(
+      "Token:",
+      token,
+      "Original TOKEN:",
+      process.env.JWT_SECRET,
+      "Decoded:",
+      decoded,
+      "Error:",
+      err
+    );
+>>>>>>> 58d1ca4 (feat: Enabling token validation in token middleware)
     if (err) {
       next(
         new CustomError(500, ["Token authentication failed."], {
@@ -32,9 +55,8 @@ export function tokenHandler(
         })
       );
     }
-
     // Si tout est bon, enregistre la demande pour une utilisation dans d'autres routes
-    req.userId = (decoded as JwtPayload).id;
+    req.user = (decoded as JwtPayload).id;
     next();
   });
 }
