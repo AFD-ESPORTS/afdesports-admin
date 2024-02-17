91718d1aacdd5b6b601c1afc9d4bb5324ee68778
Gilles COGNIN <gillescognin@gmail.com>
2024-02-17 11:35:14 +0100

    feat: Types enhancement

--- api/types/customTypes.d.ts
+++ api/types/customTypes.d.ts
@@ -1,6 +1,16 @@
+import { JwtPayload } from "jsonwebtoken";
+import { Request } from "express";
+import { RouteConfig } from "./routeConfig";
+import { Schema } from "express-validator";
+
 export interface AvailableRoute {
   routeName: String;
   routePath: String;
-  schema: Object;
-  config: Object;
+  schema: Schema;
+  config: RouteConfig;
+}
+
+export interface ExtendedRequest extends Request {
+  user?: JwtPayload;
+  routeConfig: AvailableRoute;
 }
