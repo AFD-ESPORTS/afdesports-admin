66b29a2902dd788e108ebdfceac60148a6a27e0d
Gilles COGNIN <gillescognin@gmail.com>
2024-02-18 22:26:39 +0100

    chore: Removing unecessary console logs and imports

--- api/db/models/associations.ts
+++ api/db/models/associations.ts
@@ -1,5 +1,3 @@
-import { sequelize } from "#db/sequelize";
-
 // Models
 import { Users } from "#services/users/users.model";
 import { AccessLevels } from "#services/accessLevels/accessLevels.model";
@@ -13,14 +11,3 @@ AccessLevels.belongsTo(Users, {
   foreignKey: "access_level",
   as: "users",
 });
-
-// sequelize.sync();
-//   .then(() => {
-//     console.log("Tables have been synced");
-//   })
-//   .catch((error) => {
-//     // console.error(
-//     //   "The API encountered an error while syncing database models :",
-//     //   error
-//     // );
-//   });
