ea9514bcbdbe944176fd5d48e45a3fed34f7e099
Gilles COGNIN <gillescognin@gmail.com>
2024-02-21 07:03:21 +0100

    feat: Creating the connection form

--- components/LoginPane.vue
+++ components/LoginPane.vue
@@ -1,7 +1,36 @@
 <template>
   <div
-    class="position-relative box-border h-32 w-32 p-4 border-1 rounded-md bg-slate-100 shadow-xl"
+    class="box-border p-8 border-2 border-slate-400/50 backdrop-blur-md bg-white/20 rounded-md drop-shadow-md hover:drop-shadow-xl"
   >
-    <div class="text-black text-center object-none object-center">Test</div>
+    <img
+      src="/img/Discord_AFDESPORTS_logo.png"
+      alt="logo"
+      class="flex items-center w-40 h-40 mx-auto mb-16 drop-shadow-xl"
+    />
+    <div class="text-black text-center object-none object-center">
+      <h1 class="text-2xl font-bold text-white mb-16">Login</h1>
+      <div class="flex justify-center">
+        <IconButton
+          icon="discord"
+          text="Se connecter avec Discord"
+          class="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded hover:shadow-xl"
+          @click="loginWithDiscord"
+          :isLoading="isLoading"
+        />
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { useDiscordStore } from "@/stores/discord";
+
+const discordStore = useDiscordStore();
+
+const isLoading = ref(false);
+
+const loginWithDiscord = () => {
+  discordStore.redirectToDiscord();
+};
+</script>
+```
