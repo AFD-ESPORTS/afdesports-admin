8c2f47ff0dcb1cbb79e8adff025b64eafcddd3d1
Gilles COGNIN <gillescognin@gmail.com>
2024-02-21 07:02:42 +0100

    feat: Creating standard loader

---
+++ components/Loader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex gap-1 animate-pulse">
+    <svg class="animate-bounce animate-delay-0 w-3 h-8 pt-3">
+      <circle cx="5" cy="5" r="5" fill="currentColor" />
+    </svg>
+    <svg class="animate-bounce animate-delay-100 w-3 h-8 pt-3">
+      <circle cx="5" cy="5" r="5" fill="currentColor" />
+    </svg>
+    <svg class="animate-bounce animate-delay-200 w-3 h-8 pt-3">
+      <circle cx="5" cy="5" r="5" fill="currentColor" />
+    </svg>
+  </div>
+</template>
