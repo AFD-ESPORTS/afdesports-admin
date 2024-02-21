c29cf23ee5956a5529a9b234bb9e3f6a63bee2ae
Gilles COGNIN <gillescognin@gmail.com>
2024-02-21 07:04:11 +0100

    feat: Creating initial discord store

--- stores/discord.ts
+++ stores/discord.ts
@@ -8,7 +8,7 @@ export const useDiscordStore = defineStore("discord", {
   }),
   actions: {
     redirectToDiscord() {
-      window.location.href = `https://discord.com/api/oauth2/authorize?client_id=${this.clientId}&redirect_uri=${this.redirectUri}&response_type=code&scope=identify%20guilds%20email`;
+      window.location.href = `${this.redirectUri}`;
     },
   },
 });
