2f484b3c64ff4c222a10e0b29b7982fd3c4c8475
Gilles COGNIN <gillescognin@gmail.com>
2024-02-21 07:02:27 +0100

    feat: Creating standard icon button

---
+++ components/IconButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <button
+    class="flex flex-wrap gap-2 items-center justify-center {{ classes }} "
+    :disabled="isLoading"
+    @click="submit"
+  >
+    <slot name="icon" class="mr-2 pr-2">
+      <Loader v-if="isLoading" />
+      <Icon :name="icon" v-else />
+    </slot>
+    <slot name="text" v-if="text">{{ text }}</slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  icon: {
+    type: String,
+    default: "",
+  },
+  text: {
+    type: String,
+    default: "",
+  },
+  classes: {
+    type: String,
+    default: "",
+  },
+  isLoading: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const { icon, text, isLoading } = toRefs(props);
+
+const submit = () => {
+  //   emit("click");
+  console.log("Click!");
+  isLoading.value = true;
+};
+</script>
