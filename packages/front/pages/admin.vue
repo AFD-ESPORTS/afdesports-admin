0f208949308e0dbc4f35cb9633618c5e18d3a359
Gilles COGNIN <gillescognin@gmail.com>
2024-03-10 11:37:41 +0100

    feat: Preparing Admin view user PopUps

--- packages/front/pages/admin.vue
+++ packages/front/pages/admin.vue
@@ -1,17 +1,22 @@
 <template>
-  <div class="flex items-center justify-center loginBackground">Admin!</div>
+  <div class="flex items-center justify-center loginBackground text-white">
+    <h1>Admin!</h1>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { onBeforeMount } from "vue";
 import { useUserStore } from "@/stores/user";
+import { usePopUpsStore } from "@/stores/popups";
 import { useRouter } from "vue-router";
 
 const userStore = useUserStore();
+const popUpsStore = usePopUpsStore();
 const router = useRouter();
 
 onBeforeMount(() => {
-  if (!userStore.isAuth) {
+  if (!userStore.isAdmin) {
+    popUpsStore.setPopUp(new Error("Vous n'êtes pas admin!"), "error");
     router.push("/login");
   }
 });
