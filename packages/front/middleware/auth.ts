849f2b57ea27613ce552903daf08ef638786ec60
Gilles COGNIN <gillescognin@gmail.com>
2024-03-12 08:54:19 +0100

    chore: Deleting unecessary folder

--- packages/front/middleware/auth.ts
+++
@@ -1,9 +0,0 @@
-import type { Middleware } from "@nuxt/types";
-
-const auth: Middleware = ({ store, redirect }) => {
-  if (!store.state.auth.isLoggedIn) {
-    return redirect("/login");
-  }
-};
-
-export default auth;
