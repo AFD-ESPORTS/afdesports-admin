6de89e404fa2aad78d52ded570d14b503d684dac
Gilles COGNIN <gillescognin@gmail.com>
2024-03-23 15:42:49 +0100

    feat: Improving Admin page

--- packages/front/pages/admin.vue
+++ packages/front/pages/admin.vue
@@ -1,35 +1,16 @@
 <template>
-  <div class="flex items-center justify-center loginBackground text-white">
-    <h1>Admin!</h1>
-    <br />
-    <div class="flex p-2 m-2">
-      <p>
-        <IconButton
-          icon="mdi:tools"
-          text="API Test"
-          class="leading-6 p-2 rounded-md bg-red-300 hover:bg-red-500 border-red-400 border-2"
-          @click="checkToken"
-          :isLoading="isLoading"
-        />
-      </p>
-    </div>
+  <div
+    class="flex w-screen h-screen bg-light-blue items-center justify-center text-white"
+  >
+    <MenusTopBar />
+    <MenusLeftBar />
+    <AdminPanelView />
   </div>
 </template>
 
 <script setup lang="ts">
 import { onBeforeMount } from "vue";
 import { adminCheck } from "~/helpers/adminCheck";
-import { testApi } from "~/api/test.api";
-
-const isLoading = ref(false);
-
-const checkToken = async () => {
-  isLoading.value = true;
-  console.log("Checking token");
-
-  await testApi();
-  isLoading.value = false;
-};
 
 onBeforeMount(() => {
   adminCheck();
