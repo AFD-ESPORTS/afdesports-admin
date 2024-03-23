5aa7c04efdfc6a779178c75e3496948202e81692
Gilles COGNIN <gillescognin@gmail.com>
2024-03-23 15:43:16 +0100

    feat: Preparing panel view with bubble loader

---
+++ packages/front/components/Admin/PanelView.vue
@@ -0,0 +1,19 @@
+<template>
+  <br />
+  <div
+    class="flex flex-col p-2 m-2 justify-center justify-items-center content-center"
+  >
+    <div>
+      <h1 class="text-blue-500 animate-pulse">
+        Chargement des données en cours
+      </h1>
+    </div>
+    <div>
+      <BubbleLoader class="text-blue-500 justify-center" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { toRefs } from "vue";
+</script>
